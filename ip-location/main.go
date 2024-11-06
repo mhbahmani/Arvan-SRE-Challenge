6661d9ea16868bf514ea01205f375dc9f681f029
MohammadHosein Bahmani <mohammad.bahmani@tapsi.cab>
2024-11-06 11:17:47 +0330

    Add using postgres in ip-location

--- ip-location/main.go
+++ ip-location/main.go
@@ -1,22 +1,103 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type IPRequest struct {
 	IP string `json:"ip"`
 }
 
+type IPResponse struct {
+	Country            string `json:"country"`
+	RequestedIP        string `json:"requested_ip"`
+	FetchedFromDatabase bool   `json:"fetched_from_database"`
+}
+
 type IPInfo struct {
 	Country string `json:"country"`
 }
 
-func getCountryByIP(ip string) (string, error) {
+func connectDB() (*sql.DB, error) {
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	return sql.Open("postgres", dsn)
+}
+
+func countryHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var ipRequest IPRequest
+	if err := json.NewDecoder(r.Body).Decode(&ipRequest); err != nil {
+		http.Error(w, "Invalid JSON input", http.StatusBadRequest)
+		return
+	}
+
+	country, fromDB, err := getCountry(ipRequest.IP)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := IPResponse{
+		Country:            country,
+		RequestedIP:        ipRequest.IP,
+		FetchedFromDatabase: fromDB,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func getCountry(ip string) (string, bool, error) {
+	db, err := connectDB()
+	if err != nil {
+		return "", false, err
+	}
+	defer db.Close()
+
+	var country string
+	err = db.QueryRow("SELECT country FROM ip_locations WHERE ip = $1", ip).Scan(&country)
+	if err == nil {
+		return country, true, nil
+	}
+	if err != sql.ErrNoRows {
+		return "", false, err
+	}
+
+	country, err = fetchCountryFromAPI(ip)
+	if err != nil {
+		return "", false, err
+	}
+
+	_, err = db.Exec("INSERT INTO ip_locations (ip, country) VALUES ($1, $2)", ip, country)
+	if err != nil {
+		return "", false, err
+	}
+
+	return country, false, nil
+}
+
+func fetchCountryFromAPI(ip string) (string, error) {
 	url := fmt.Sprintf("http://ip-api.com/json/%s", ip)
+	// TODO: Add alternative
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", fmt.Errorf("failed to get IP info: %v", err)
@@ -35,29 +116,6 @@ func getCountryByIP(ip string) (string, error) {
 	return info.Country, nil
 }
 
-func countryHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var ipRequest IPRequest
-	if err := json.NewDecoder(r.Body).Decode(&ipRequest); err != nil {
-		http.Error(w, "Invalid JSON input", http.StatusBadRequest)
-		return
-	}
-
-	country, err := getCountryByIP(ipRequest.IP)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	response := map[string]string{"country": country}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
-}
-
 func main() {
 	http.HandleFunc("/get-country", countryHandler)
 
@@ -67,4 +125,3 @@ func main() {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
-
